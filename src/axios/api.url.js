f290290411aa8471f3eb80a2a3048d053ba6e4fa
developerqz <qizhuang1538026893@gmail.com>
2022-04-13 18:59:50 +0800

    修改域名

--- src/axios/api.url.js
+++ src/axios/api.url.js
@@ -1,13 +1,13 @@
 
 const ENV = process.env.NODE_ENV
 export default {
-  // DOMAIN: 'http://sr.nqsoft.net',
-  DOMAIN: 'http://localhost',
+  DOMAIN: 'http://www.wxljcee.vip',
+  // DOMAIN: 'http://localhost',
   // baseURL: ENV == 'development'?'http://sr.nqsoft.net':'http://sr.nqsoft.net',
   // baseURL: 'http://localhost:8070',
   /* Util API */
-  // baseURL: 'http://sr.nqsoft.net',
-  baseURL: 'http://localhost',
+  baseURL: 'http://www.wxljcee.vip',
+  // baseURL: 'http://localhost',
   util: {
     image: '/util/image.html' // 图片上传
   }
